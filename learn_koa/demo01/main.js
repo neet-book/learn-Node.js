3459925afe83f41c1d6b90903f915f821084b6ea
erila <yanoerika@163.com>
2020-03-16 21:43:10 +0800

    koa 路由

---
+++ learn_koa/demo01/main.js
@@ -0,0 +1,48 @@
+// 原生模块
+const path = require('path')
+
+// 引入koa
+const Koa = require('koa')
+const Router = require('koa-router')
+const views = require('koa-views')
+
+// 引入模板引擎
+const ejs = require('ejs')
+
+// 创建应用
+const app = new Koa()
+// 创建路由
+const router = new Router()
+
+// 注册模板引擎
+app.use(views(path.resolve(__dirname, 'views/'), {
+  // 配置views
+  // 设置模板文件使用的哪种渲染的模板引擎，如果设置了此项，模板文件扩展名应该是html而不是ejs
+  map: { html: 'ejs' },
+  // 设置模板文件的扩展名，如设置了此项则渲染模板时不需要指定模板文件的扩展名
+  extension: 'ejs',
+}))
+
+// 配置模板公共数据
+app.use(async (ctx, next) => {
+  // 在ctx.state中定义多个路由其模板所需要使用的公共变量
+  ctx.state.date = new Date().toLocaleDateString()
+  await next()
+})
+
+// 配置路由
+router.get('/', async  (ctx) => {
+  // 渲染模板
+  // 如果注册views时没有设置autoRender,那么不需要手动将结果返回
+  await ctx.render('index', { module: 'ejs' })
+})
+
+// 注册路由
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+// 启动应用
+app.listen(3000, () => {
+  console.log('Server runing at http://localhost:3000')
+})
+
