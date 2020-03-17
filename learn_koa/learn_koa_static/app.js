9231b1689965dbf91d5c7368905a06cf31cda6c5
erila <yanoerika@163.com>
2020-03-17 23:07:02 +0800

    登录

--- learn_koa/learn_koa_static/app.js
+++ learn_koa/learn_koa_static/app.js
@@ -6,6 +6,7 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const serve = require('koa-static')
 const views = require('koa-views')
+const bodypaser = require('koa-bodyparser')
 
 // 创建实例
 const app = new Koa()
@@ -17,13 +18,38 @@ app.use(views(path.resolve(__dirname, 'views'), { extension: 'ejs' }))
 // 注册static
 app.use(serve(path.resolve(__dirname, 'static')))
 
+// 注册bodypaser
+app.use(bodypaser())
+
+app.use(async (ctx, next) => {
+  console.log(ctx.request.body)
+
+  await next()
+})
+
 // 配置路由
-router.get('/', async (ctx) => {
+
+// 首页
+router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     module: 'ejs',
     date: new Date().toLocaleDateString()
   })
+
+
 })
+
+// 登录
+router.post('/login', async (ctx) => {
+  console.log(ctx.request.body)
+  ctx.status = 200
+  ctx.set({ Accept: 'application/json' })
+  ctx.cookies.set('username', ctx.request.body.userName)
+  ctx.app.on('data', data => console.log(data))
+  let body =JSON.stringify({ token: '0oo091jdfhe' })
+  ctx.body = body
+})
+
 
 // 注册路由
 app.use(router.routes())
--- learn_koa/learn_koa_static/static/script/main.js
+++ learn_koa/learn_koa_static/static/script/main.js
@@ -1,2 +1,49 @@
-const loginForm = document.getElementById('user-login-form')
+const loginForm = document.forms[0]
 const submit = document.getElementById('loging-btn')
+
+submit.addEventListener('click', async (event) => {
+  // event.preventDefault()
+  const re =  await fetch('/login', {
+    method: 'post',
+    body: JSON.stringify(serialize(loginForm))
+  })
+
+  if (re.status !== 200) return console.log('登录失败：', re)
+  console.log(re)
+})
+
+
+loginForm.onsubmit = () => {
+  console.log('sub')
+}
+ 
+
+function serialize(form) {
+  let data = {}
+  for (item of form) {
+    if (item.tagName.toLowerCase() !== 'button' && item.type !== 'button') {
+      let key = camelCase(item.name)
+      
+      switch(item.type) {
+        case 'checkbox': 
+          data[key] = item.checked
+        break
+        case 'radio':
+          data[key] = item.value
+        break
+        default:
+          data[key] = item.value
+      }
+    }
+  }
+  console.log(data)
+  return data
+}
+function camelCase(key) {
+  return key.split('-').map((word, index) => {
+    if (index === 0) return word
+    let wordArr = word.split('')
+    wordArr[0] = word[0].toUpperCase()
+    return wordArr.join('')
+  }).join("")
+}
