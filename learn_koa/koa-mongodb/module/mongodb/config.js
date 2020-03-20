76df2b361f72bffb1b98c8f41779c9a32465c40f
NEET-Book <yanoerika@163.com>
2020-03-20 15:51:46 +0800

    Please enter the commit message for your changes. Lines starting

--- learn_koa/koa-mongodb/module/mongodb/config.js
+++ learn_koa/koa-mongodb/module/mongodb/config.js
@@ -4,7 +4,8 @@ const config = {
   user: 'koa',
   pwd: '123456',
   host: 'localhost',
-  port: '27017'
+  port: '27017',
+  reconnect: 3
 }
 
 module.exports = initConfig(config)
@@ -13,7 +14,7 @@ module.exports = initConfig(config)
 function initConfig(config) {
   if (!config.host) throw new Error('初始化配置错误: 必须传入host')
   if (!config.port) config.port = 27017
-
+  if (!config.reconnect) config.reconnect = 3
   // 创建url
   if (config.user && config.pwd && config.dbname) {
     config.url = 'mongodb://' +
--- learn_koa/koa-mongodb/module/mongodb/database.js
+++ learn_koa/koa-mongodb/module/mongodb/database.js
@@ -1,30 +1,52 @@
 const MongoClient = require('mongodb')
 const config = require('./config')
 
+const assert = require('assert')
+
 class Database {
   constructor() {
-    // this.connect()
+    this.database = Database.connect()
   }
 
   // 链接数据库
-  connect() {
-    return new Promise((res, rej) => {
+  static async connect() {
+    return await new Promise((res, rej) => {
       MongoClient.connect(config.url, (err, client) => {
         if (err) return rej(err)
         console.log('链接数据库成功')
         res(client.db(config.dbname))
       })
     })
-  }
-  // 查找数据
-  async find(colle, query) {
-    const db = await this.connect().catch(err => {
-      console.log('数据库链接失败：', err)
+    .catch(err => {
+      console.log('数据库连接失败：', err.message)
+      return false
     })
+  }
 
+  // 重连数据库
+  async reconnect(count = 0) {
+    if(config.reconnect < count) return console.log('Error: 连接数据库失败超出限制')
+    return Database.connect().then(db => {
+      // 1秒后再次重连
+      if (!db) {
+        return setTimeout(()=> {
+          this.reconnect(++count)
+        }, 2000)
+      }
+      this.database = db
+      return db
+    })
+  }
+
+  // 查找数据
+  async find(colle, query, ) {
+
+    const db = await this.database
+    // 检查是否连接成功
+    if (!db) db = await this.reconnect()
+    // 查询
     return await new Promise((res, rej) => {
       db.collection(colle).find(query).toArray((err, docs) => {
-        // db.close()
         if (err) return rej(err)
         res(docs)
       })
@@ -33,5 +55,9 @@ class Database {
 }
 console.log(config.url)
 const db = new Database
-console.log(db.find('test', {}))
+
+db.find('module', {}).then(re => {
+  console.log(re)
+})
+
 
